d1393e5dc5f9e5a590d763adc94e5443b960e3a1
Overlord <maksimstenakin@gmail.com>
2025-10-10 19:29:57 +0400

    commit

---
+++ division.php
@@ -0,0 +1,26 @@
+<?php
+
+function divisionScript() {
+    fwrite(STDOUT, "Введите первое число: ");
+    $num1_str = trim(fgets(STDIN));
+    fwrite(STDOUT, "Введите второе число: ");
+    $num2_str = trim(fgets(STDIN));
+    if (!is_numeric($num1_str) || !is_numeric($num2_str)) {
+        fwrite(STDERR, "Введите, пожалуйста, число" . PHP_EOL);
+        return 1;
+    }
+    $num1 = (int)$num1_str;
+    $num2 = (int)$num2_str;
+    if ($num2 == 0) {
+        fwrite(STDERR, "Делить на 0 нельзя" . PHP_EOL);
+        return 1;
+    }
+    
+    // Выполнение деления и вывод результата
+    $result = $num1 / $num2;
+    fwrite(STDOUT, "Результат деления: " . $result . PHP_EOL);
+    return 0;
+}
+exit(divisionScript());
+
+?>
